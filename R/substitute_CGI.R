c4973d80ac0359bfdd984b182f6795b5a65791fe
Andrea Mariani <ieo5776@ieo.it>
2024-07-23 11:23:21 +0200

    adding package repo

--- R/substitute_CGI.R
+++ R/substitute_CGI.R
@@ -1,3 +1,40 @@
+#' Add a user provided CG Island anywhere in the sequence. It operates on a sequence and its corresponding orientation
+#'
+#' @param seqs A list of Biostrings sequences to perform operations on
+#' @param orientation A vector containing the orientation, "+", or "-" of the sequence.
+#' @param seq_to_add The string to be added
+#' @param offset How many bps from the center of the sequence should the sequence be added
+#' @param direction Which direction the substitutions needs to happen. Valid inputs are "upstream" or "downstream".
+#' This is affected by the orientation
+#'
+#' @return A DNAStringSet object
+#' @import Biostrings
+#' @export
+#'
+#' @examples
+#' random_seq <- Biostrings::DNAStringSet("ATCGTATCGATCGATCGATCATCGATCGATCGATCAG")
+#' OR <- c("+")
+#' substitute_CGI(seqs=random_seq, orientation=OR, seq_to_add="CGCG", offset=10, direction="upstream")
+
+substitute_CGI <- function(seqs, orientation, seq_to_add, offset, direction=c("upstream", "downstream")) {
+  Biostrings::DNAStringSet(
+    mapply(sub_CGI, seqs, orientation, as.character(seq_to_add), offset, direction))
+}
+
+
+#' Utilily for substitute_CGI
+#'
+#' @param seqs A Biostrings sequence to perform operations on
+#' @param orientation The orientation of the sequence, "+", or "-".
+#' @param seq_to_add The string to be added
+#' @param offset How many bps from the center of the sequence should the sequence be substituted.
+#' @param direction Which direction the substitutions needs to happen. Valid inputs are "upstream" or "downstream".
+#' This is affected by the orientation
+#'
+#' @return A DNAString object
+#' @import Biostrings
+#' @export
+#'
 sub_CGI <- function(seqs, orientation, seq_to_add, offset, direction=c("upstream", "downstream")) {
   # convert the DNA strings to a char vector
   dna_chars <- as.character(seqs)
@@ -28,10 +65,3 @@ sub_CGI <- function(seqs, orientation, seq_to_add, offset, direction=c("upstream
   #convert to DNAString
   return(Biostrings::DNAString(dna_chars))
 }
-
-
-substitute_CGI <- function(seqs, orientation, seq_to_add, offset, direction=c("upstream", "downstream")) {
-  Biostrings::DNAstringSet(
-    mapply(sub_CGI, seqs, orientation, as.character(seq_to_add), offset, direction))
-}
-
